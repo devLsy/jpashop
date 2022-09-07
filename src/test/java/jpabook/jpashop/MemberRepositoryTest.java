474259ee1a8f6a5da304e526793ba3b3852fe5ae
leesangyeop <devlsy86@mncnext.co.kr>
2022-09-07 12:44:37 +0900

    회원 서비스 개발 중

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -17,16 +17,10 @@ class MemberRepositoryTest {
     @Rollback(false)
     public void testMember() throws Exception {
         //givin
-        Member member = new Member();
-        member.setUsername("아이언맨");
+
         //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
+
         //then
-        Assertions.assertThat(findMember.getId().equals(member.getId()));
-        Assertions.assertThat(findMember.getUsername().equals(member.getUsername()));
-        Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member :: " + (findMember == member));
 
     }
 
