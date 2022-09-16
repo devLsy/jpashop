32a359a1008c252880096a3a61b4fc07643fd3f7
leesangyeop <devlsy86@mncnext.co.kr>
2022-09-16 12:55:44 +0900

    ing~~~

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    // 주문 등록
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    // 주문 단건 조회
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // 주문 목록 조회
+//    public List<Order> findAll(OrderSearch orderSearch) {
+
+//    }
+}
